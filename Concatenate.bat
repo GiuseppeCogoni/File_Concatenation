2dc56fdd1c1bad5163efa9eeda3c047756663ab4
GiuseppeCogoni <giuseppe.cogoni@pfizer.com>
2021-05-11 15:34:18 -0400

    minor changes on the batch script

--- Concatenate.bat
+++ Concatenate.bat
@@ -7,29 +7,27 @@ set "psCommand="(new-object -COM 'Shell.Application')^
 :folder_sel
 for /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "folder=%%I"
 
-IF "!folder!" == "" (
-echo No folder selected^^!
-echo.
-
-SET /P redo="Would you like to try again [Y/n]? "
-IF "!redo!" EQU "Y" (
-GOTO :folder_sel
-) ELSE (
-exit 0
-)
-
-) ELSE (
-echo You chose !folder!...
-echo.
+if "!folder!" == "" (
+	echo No folder selected^^!
+	echo.
+	set /P redo="Would you like to try again [Y/n]? "
+	if "!redo!" == "Y" (
+		goto :folder_sel
+	) else (
+		exit 0
+	)
+) else (
+	echo You chose !folder!...
+	echo.
 )
 
 :string
-SET /P name="Type an output filename: "
-IF "!name!" == "" (
-echo You entered an empty string...
-GOTO :string
-) ELSE (
-echo You typed: !name!
+set /P name="Type an output filename: "
+if "!name!" == "" (
+	echo You entered an empty string...
+	goto :string
+) else (
+	echo You typed: !name!
 )
 
 set "psCommand="(new-object -COM 'Shell.Application')^
@@ -38,37 +36,41 @@ set "psCommand="(new-object -COM 'Shell.Application')^
 :folder_out
 for /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "out_folder=%%I"
 
-IF "!out_folder!" == "" (
-echo No folder selected^^!
-echo.
+if "!out_folder!" == "" (
+	echo No folder selected^^!
+	echo.
 
-SET /P redo="Would you like to try again [Y/n]? "
-IF "!redo!" EQU "Y" (
-GOTO :folder_out
-) ELSE (
-exit 0
+	set /P redo="Would you like to try again [Y/n]? "
+	if "!redo!" == "Y" (
+		goto :folder_out
+	) else (
+		exit 0
+	)
+
+) else (
+	echo You chose !out_folder!...
+	echo.
 )
 
-) ELSE (
-echo You chose !out_folder!...
-echo.
-)
-
-REM set count to 1
+rem set count to 1
 set cnt=1
+:heads
+set /P head="Type the number of header rows in the source files: "
+echo.
+set /P isok="Is !head! the number of header row(s) correct [Y/n]? "
+if not "!isok!" == "Y" goto :heads
 
-for %%i in (*.txt) do (
-REM if count is 1 it's the first time running
-  if !cnt!==1 (
-REM push the entire file complete with header into the combined file
-    for /f "delims=" %%j in ('type "%%i"') do echo %%j >> !out_folder!\!name!.csv
-REM otherwise, make sure we're not working with the combined file and
-  ) else if %%i NEQ !out_folder!\!name!.csv (
-REM push the file without the header into the combined file
-    for /f "skip=1 delims=" %%j in ('type "%%i"') do echo %%j >> !out_folder!\!name!.csv
-  )
-REM increment count by 1
-  set /a cnt+=1
+for %%i in ("!folder!"\*.txt) do (
+	rem if count is 1 it's the first time running
+	if !cnt!==1 (
+		rem push the entire file complete with header into the combined csv-file
+		for /f "delims=" %%j in ('type "%%i"') do echo %%j >> !out_folder!\!name!.csv
+	rem otherwise, make sure we're not working with the combined file and
+	) else if %%i NEQ !out_folder!\!name!.csv (
+		rem push the file without the header into the combined csv-file
+		for /f "skip=%head% delims=" %%j in ('type "%%i"') do echo %%j >> !out_folder!\!name!.csv
+	)
+	rem increment count by 1
+	set /a cnt+=1
 )
-pause
 exit 0
